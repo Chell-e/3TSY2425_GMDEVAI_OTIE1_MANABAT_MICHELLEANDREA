99b3b7a44e2d234f41e8a6aabe855b60a6dec67d
Chelle <michelleandrea.manabat@benilde.edu.ph>
2025-08-08 04:26:52 +0800

    Module 2
    Pretty much done!

---
+++ Module 2_Car Simulation/Assets/Scripts/FollowWaypoint.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWaypoint : MonoBehaviour
+{
+    //public GameObject[] waypoints;
+
+    public UnityStandardAssets.Utility.WaypointCircuit circuit;
+    public int currentWaypointIndex = 0;
+
+    private float speed = 15f;
+    private float rotationSpeed = 3f;
+    private float accuracy = 1f;
+    void Start()
+    {
+        //waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+    }
+
+    void LateUpdate()
+    {
+        if (circuit.Waypoints.Length == 0) return;
+
+        GameObject currentWayPoint = circuit.Waypoints[currentWaypointIndex].gameObject;
+
+        Vector3 lookAtWaypoint = new Vector3(currentWayPoint.transform.position.x, 
+                                            this.transform.position.y, 
+                                            currentWayPoint.transform.position.z);
+
+        Vector3 direction = lookAtWaypoint - this.transform.position;
+
+        if (direction.magnitude < 1.0f)
+        {
+            currentWaypointIndex++;
+
+            if (currentWaypointIndex >= circuit.Waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                                Quaternion.LookRotation(direction), 
+                                                Time.deltaTime * rotationSpeed);
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ Module 2_Car Simulation/Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovement : MonoBehaviour
+{
+    public Transform goal;
+
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float rotationSpeed;
+
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deceleration;
+
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+
+    [SerializeField] private float breakAngle;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x, 
+                                            this.transform.position.y, 
+                                            goal.position.z);
+        Vector3 direction = lookAtGoal - this.transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                            Quaternion.LookRotation(direction), 
+                                            Time.deltaTime * rotationSpeed);
+
+        if (Vector3.Angle(goal.forward, this.transform.forward) > breakAngle && speed > 2)
+        {
+            speed = Mathf.Clamp(speed - (deceleration * Time.deltaTime), minSpeed, maxSpeed);
+        }
+        else
+        {
+             speed = Mathf.Clamp(speed + (acceleration * Time.deltaTime), minSpeed, maxSpeed);
+        }
+            this.transform.Translate(0, 0, speed);
+    }
+}
